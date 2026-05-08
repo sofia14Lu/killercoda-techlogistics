4a9a47ce32430aee1d89666e3c533fe3459d1514
sofia <sofiaidk298@gmail.com>
2026-05-08 11:05:25 -0500

    Update foreground.sh

--- almacenamiento-nube/foreground.sh
+++ almacenamiento-nube/foreground.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y awscli curl
+# Actualizar e instalar herramientas necesarias
+apt-get update && apt-get install -y awscli curl zip
 
+# Crear directorios de trabajo
 mkdir -p /home/usuario/datos-locales
 mkdir -p /home/usuario/minio-data
 
+# Mensaje de bienvenida
 echo "Entorno preparado para TechLogistics S.A."
